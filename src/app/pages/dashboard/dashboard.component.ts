0a54d0b1f187e1714171f8c393923f9cdbd2f0fb
LeHa2505 <leha25052001@gmail.com>
2023-01-19 18:46:19 +0700

    filter

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -12,8 +12,6 @@ import { ChartOptions, ChartType, ChartDataset, Scale } from 'chart.js';
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.less'],
 })
-
-
 export class DashboardComponent implements OnInit {
   listBreadCrumb = [
     {
@@ -101,29 +99,40 @@ export class DashboardComponent implements OnInit {
     console.log(e);
   }
   constructor() {}
-
   inOutcomeSelect = 'income';
-  timeSelected = 'year';
   monthSelected = '1';
   yearSelected = '2023';
   monthArray = [
-    {label: 'Tháng 1', value: '1'}, 
-    {label: 'Tháng 2', value: '2'}, 
-    {label: 'Tháng 3', value: '3'}, 
-    {label: 'Tháng 4', value: '4'}, 
-    {label: 'Tháng 5', value: '5'}, 
-    {label: 'Tháng 6', value: '6'}, 
-    {label: 'Tháng 7', value: '7'}, 
-    {label: 'Tháng 8', value: '8'}, 
-    {label: 'Tháng 9', value: '9'}, 
-    {label: 'Tháng 10', value: '10'}, 
-    {label: 'Tháng 11', value: '11'}, 
-    {label: 'Tháng 12', value: '12'}, 
-  ]
+    { label: 'Tháng 1', value: '1' },
+    { label: 'Tháng 2', value: '2' },
+    { label: 'Tháng 3', value: '3' },
+    { label: 'Tháng 4', value: '4' },
+    { label: 'Tháng 5', value: '5' },
+    { label: 'Tháng 6', value: '6' },
+    { label: 'Tháng 7', value: '7' },
+    { label: 'Tháng 8', value: '8' },
+    { label: 'Tháng 9', value: '9' },
+    { label: 'Tháng 10', value: '10' },
+    { label: 'Tháng 11', value: '11' },
+    { label: 'Tháng 12', value: '12' },
+  ];
 
-  yearArray = ['2018', '2019', '2020', '2021', '2022', '2023'];
+  
+  yearArray = ['2017', '2018', '2019', '2020', '2021', '2022', '2023'];
+  timeSelected = 'year';
+  isDisabled = true;
+  onChangeTime(newTimeSelected) {
+    console.log(newTimeSelected);
+    this.timeSelected = newTimeSelected;
+    if (newTimeSelected === 'year') {
+      this.isDisabled = true;
+    } else this.isDisabled = false;
+    console.log(this.isDisabled);
+    
+    // ... do other stuff here ...
+  }
+
   ngOnInit(): void {
     console.log(this.inOutcomeSelect);
-    
   }
 }
