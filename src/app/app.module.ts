220561e48a7cf4f4aba4b17f42ce77ab6ec84ff9
LeHa2505 <leha25052001@gmail.com>
2023-01-29 15:45:44 +0700

    login interface

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,6 +24,7 @@ import { CommonModule } from '@angular/common';
 import { AuthenticationModule } from './authentication/authentication.module';
 
 
+
 registerLocaleData(vi);
 
 @NgModule({
--- src/app/authentication/login/login.component.ts
+++ src/app/authentication/login/login.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import {
+  UntypedFormBuilder,
+  UntypedFormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.less']
+  styleUrls: ['./login.component.less'],
 })
 export class LoginComponent {
+  validateForm!: UntypedFormGroup;
 
+  submitForm(): void {
+    console.log('submit', this.validateForm.value);
+  }
+
+  constructor(private fb: UntypedFormBuilder) {}
+
+  ngOnInit(): void {
+    this.validateForm = this.fb.group({
+      email: [null, [Validators.required]],
+      password: [null, [Validators.required]],
+      remember: [false],
+    });
+  }
 }
